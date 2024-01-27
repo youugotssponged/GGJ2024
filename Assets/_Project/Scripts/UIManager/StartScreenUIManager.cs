d453faa4577933d6ddd01827c4c18e3c620ff43e
Nathan-Dewhurst <nathandewhurst1998@gmail.com>
2024-01-27 00:35:08 +0000

    Switching between start screen buttons and settings buttons working.

--- Assets/_Project/Scripts/UIManager/StartScreenUIManager.cs
+++ Assets/_Project/Scripts/UIManager/StartScreenUIManager.cs
@@ -5,17 +5,29 @@ using UnityEngine.UIElements;
 
 public class StartScreenUIManager : MonoBehaviour
 {
+    [SerializeField]
+    UIDocument SettingsButtonsUIDocument;
+    UIDocument StartButtonsUIDocument;
 
-    // UI Elements
-    VisualElement Root;
+    VisualElement StartScreenRoot;
+    VisualElement SettingsRoot;
+
     // Start is called before the first frame update
     void Start()
     {
-        Root = GetComponent<UIDocument>().rootVisualElement;
-        Root.Q<Button>("StartButton").clicked += StartButton_clicked;
-        Root.Q<Button>("SettingsButton").clicked += SettingsButton_clicked;
-        Root.Q<Button>("ExitButton").clicked += ExitButton_clicked;
+        // Setting up StartScreen button events
+        StartButtonsUIDocument = GetComponent<UIDocument>();
+        StartScreenRoot = StartButtonsUIDocument.rootVisualElement;
+        StartScreenRoot.Q<Button>("StartButton").clicked += StartButton_clicked;
+        StartScreenRoot.Q<Button>("SettingsButton").clicked += SettingsButton_clicked;
+        StartScreenRoot.Q<Button>("ExitButton").clicked += ExitButton_clicked;
+
+        // Setting up Settings menu events.
+        SettingsRoot = SettingsButtonsUIDocument.rootVisualElement;
+        SettingsRoot.style.display = DisplayStyle.None;
+        SettingsRoot.Q<Button>("BackButton").clicked += BackButton_clicked;
     }
+
 
     private void StartButton_clicked()
     {
@@ -25,11 +37,19 @@ public class StartScreenUIManager : MonoBehaviour
     private void SettingsButton_clicked()
     {
         // Hide the default UI, and show settigns UI.
+        StartScreenRoot.style.display = DisplayStyle.None;
+        SettingsRoot.style.display = DisplayStyle.Flex;
     }
 
     private void ExitButton_clicked()
     {
         // Close the game
         Application.Quit();
+    }
+    private void BackButton_clicked()
+    {
+        // Hide Settings, and show start screen buttons.
+        SettingsRoot.style.display = DisplayStyle.None;
+        StartScreenRoot.style.display = DisplayStyle.Flex;
     }
 }
