b012c191cf7974beac42364681833609e78267c3
Niall <23546468@edgehill.ac.uk>
2024-01-27 13:39:00 +0000

    Filled out map scenery, added door colour changing and door handle animation

--- Assets/_Project/Scripts/Doors/DoorController.cs
+++ Assets/_Project/Scripts/Doors/DoorController.cs
@@ -1,7 +1,9 @@
 using DG.Tweening;
+using DG.Tweening.Plugins.Options;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DoorController : MonoBehaviour
@@ -11,19 +13,20 @@ public class DoorController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        OpenClose();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             OpenClose();
+            ChangeColour(Color.yellow);
         }
     }
 
-    private void OpenClose(float rotateSpeed = 2.0f) //0.5f seems like it would be good for slamming
+    public void OpenClose(float rotateSpeed = 2.0f) //0.5f seems like it would be good for slamming
     {
         if (transform.rotation.eulerAngles.y % 90 != 0) return; //only opens/closes if not already
         if (Open)
@@ -34,9 +37,26 @@ public class DoorController : MonoBehaviour
         } else
         {
             //open
+            var children = GetComponentsInChildren<Transform>();
+            var handle = children.FirstOrDefault(x => x.gameObject.name == "Handle");
+            if (handle != null)
+            {
+                Debug.Log(handle.gameObject.name);
+                handle.transform.DOLocalRotate(new Vector3(0, -30, 0), 0.33f).OnComplete(() =>
+                {
+                    handle.transform.DOLocalRotate(new Vector3(0, 0, 0), 0.33f);
+                });
+            }
             var newVector = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + 90, transform.rotation.eulerAngles.z);
             transform.DORotate(newVector, rotateSpeed);
         }
         Open = !Open;
+    }
+
+    public void ChangeColour(Color colour)
+    {
+        var material = GetComponent<Renderer>().material;
+        if (material != null)
+            material.color = colour;
     }
 }
